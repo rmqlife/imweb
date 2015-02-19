5f53317778d52a94e2863d73c9becbb3273d4d05
rmqlife <rmqlife@gmail.com>
2015-02-19 18:36:22 +0800

    init

---
+++ imweb.py
@@ -0,0 +1,42 @@
+from flask import Flask,request,render_template,url_for,redirect
+app=Flask(__name__)
+
+SITE_ADDR="http://piwan.me:5000"
+LIB_FOLDER='pics'
+app.config['LIB_FOLDER']=LIB_FOLDER
+
+ALLOWED_EXTENSIONS=set(['jpg','bmp','jpeg','png'])
+def allowed_files(filename):
+	return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+
+def qr(text):
+	import qrcode
+	print text
+	img=qrcode.make(text)
+	img.save("static/qrcode.png")
+
+@app.route('/')
+def index():
+	qr(SITE_ADDR+url_for('index'))
+	return render_template("index.html") 
+
+@app.route('/show/')
+def show():
+	request.args.get('link',0,type=string)
+
+@app.route('/ls/')
+def list_file():
+	output=""
+	print app.config['LIB_FOLDER']
+	from os import walk, path
+	for root,dirs,files in walk(app.config['LIB_FOLDER']):
+		for filename in files:
+			full_path=path.join(root,filename)
+			if allowed_files(filename):
+				output=output+'''<p><a href="{link}">{name}</a></p>'''\
+				.format(link=url_for('static',filename=full_path),name=filename)
+	return output
+
+
+if __name__ == '__main__':
+	app.run(host="0.0.0.0",port=5000,debug=True)
